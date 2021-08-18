48dd6726012eea58ed99a0160e172bd3c8942df9
1paragraph <1paragraph@github.com>
2021-08-18 10:52:28 +0300

    yolo fix

--- scripts/workshop.py
+++ scripts/workshop.py
@@ -3,10 +3,10 @@ import cv2
 from cars_parser import CarsParser
 
 m = nvbs_models.NvbsCarModel(
-    classifier=nvbs_models.CarsClassifier('./model_weights/b4.pt'),
-    detector=nvbs_models.CarsDetector('./model_weights/yolov5m.pt'),
-    ocr=nvbs_models.PanelDigitsDetector('./model_weights/class_resnet18d.pt',
-                                        './model_weights/bbox_model_resnet18d.pt')
+    classifier=nvbs_models.CarsClassifier('../model_weights/b4.pt'),
+    detector=nvbs_models.CarsDetector('../model_weights/yolov5m.pt'),
+    ocr=nvbs_models.PanelDigitsDetector('../model_weights/class_resnet18d.pt',
+                                        '../model_weights/bbox_model_resnet18d.pt')
     )
 
 cp = CarsParser(m)
